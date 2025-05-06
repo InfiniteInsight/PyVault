e879c89d4ca68587fa2748cd394193e8cfdf3847
Evan Nevermore <InfiniteInsight@gmail.com>
2025-05-05 23:16:31 -0400

    aligning filepaths and working out container issues

--- backend/src/pyvault/main.py
+++ backend/src/pyvault/main.py
@@ -1,12 +1,14 @@
 import uvicorn
 
-from .config import config
+from src.pyvault.api import app
+from src.pyvault.config import config
 
 
 def main():
     """Run the API server"""
     uvicorn.run(
-        "pyvault.api:app",
+        # "pyvault.api:app",
+        app,
         host=config.api.host,
         port=config.api.port,
         reload=config.api.debug,
